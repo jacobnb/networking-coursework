4f908447fa3a4c8f2e4f78910e3218fa0a58ac7a
jacob biederman <jacob.biederman@mymail.champlain.edu>
2020-01-16 10:29:07 -0500

    RakNet tutorial 2

--- networking/networking.cpp
+++ networking/networking.cpp
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include "RakNet/RakPeerInterface.h"
+#include <string.h>
+#include "RakNet/MessageIdentifiers.h"
 
 #define RackNet RakNet
 
@@ -16,6 +18,7 @@ int main(void)
 	char str[512];
 	RakNet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
 	bool isServer;
+	RakNet::Packet* packet;
 
 	printf("(C) or (S)erver?\n");
 	fgets(str, 512, stdin);
@@ -32,7 +35,70 @@ int main(void)
 	}
 
 
-	// TODO - Add code body here
+
+	if (isServer)
+	{
+		printf("Starting the server.\n");
+		// We need to let the server accept incoming connections from the clients
+		peer->SetMaximumIncomingConnections(MAX_CLIENTS);
+	}
+	else {
+		printf("Enter server IP or hit enter for 127.0.0.1\n");
+		fgets(str, 512, stdin);
+		if (str[0] == 0) {
+			strcpy(str, "127.0.0.1");
+		}
+		printf("Starting the client.\n");
+		peer->Connect(str, SERVER_PORT, 0, 0);
+
+	}
+
+	while (1)
+	{
+		for (packet = peer->Receive(); packet; peer->DeallocatePacket(packet), packet = peer->Receive())
+		{
+			switch (packet->data[0])
+			{
+			case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+				printf("Another client has disconnected.\n");
+				break;
+			case ID_REMOTE_CONNECTION_LOST:
+				printf("Another client has lost the connection.\n");
+				break;
+			case ID_REMOTE_NEW_INCOMING_CONNECTION:
+				printf("Another client has connected.\n");
+				break;
+			case ID_CONNECTION_REQUEST_ACCEPTED:
+				printf("Our connection request has been accepted.\n");
+				break;
+			case ID_NEW_INCOMING_CONNECTION:
+				printf("A connection is incoming.\n");
+				break;
+			case ID_NO_FREE_INCOMING_CONNECTIONS:
+				printf("The server is full.\n");
+				break;
+			case ID_DISCONNECTION_NOTIFICATION:
+				if (isServer) {
+					printf("A client has disconnected.\n");
+				}
+				else {
+					printf("We have been disconnected.\n");
+				}
+				break;
+			case ID_CONNECTION_LOST:
+				if (isServer) {
+					printf("A client lost the connection.\n");
+				}
+				else {
+					printf("Connection lost.\n");
+				}
+				break;
+			default:
+				printf("Message with identifier %i has arrived.\n", packet->data[0]);
+				break;
+			}
+		}
+	}
 
 	RakNet::RakPeerInterface::DestroyInstance(peer);
 
