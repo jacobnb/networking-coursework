b27a02cd6635563f51a08b52a78de96f0ff27b4e
Michael Zheng <michael.zheng@mymail.champlain.edu>
2020-02-09 21:58:02 -0500

    added architecture and added functionality
    Added tic tac toe

---
+++ NetWorkedBoardGame/Assets/Scripts/StartScene.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StartScene : MonoBehaviour
+{
+	private static StartScene instance;
+
+	public static StartScene Instance
+	{
+		get{ return instance; }
+	}
+
+	private void Awake()
+	{
+		if(instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+
+		instance = this;
+		
+	}
+
+	public Toggle isServerToggle;
+	public InputField IPinput;
+
+	bool networkLinked = false;
+	public bool isServer = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if(networkLinked == false && Input.GetKeyDown(KeyCode.Return))
+		{
+			isServer = isServerToggle.isOn;
+			if(isServer)
+			{
+				//move to next scene
+			}
+	
+
+			string ipaddress = IPinput.text;
+			//TODO: INIT NETWORK	
+			/*
+			if(connect to network and successful)
+			{
+				networkLinked = true;
+				//move to next scene
+			}
+			*/
+		}
+    }
+}
