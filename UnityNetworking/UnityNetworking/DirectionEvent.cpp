96335e362dc36e3fd5fa4c605b5c217490ba5cf9
MZ <michael.zheng@mymail.champlain.edu>
2020-02-13 10:49:48 -0500

    Initial Work on event handling
    Created event classes

---
+++ UnityNetworking/UnityNetworking/DirectionEvent.cpp
@@ -0,0 +1,1 @@
+#include "DirectionEvent.h"
---
+++ UnityNetworking/UnityNetworking/DirectionEvent.h
@@ -0,0 +1,13 @@
+#ifndef DIRECTIONEVENT_H
+#define DIRECTIONEVENT_H
+
+#include "Event.h"
+
+class DirectionEvent : Event
+{
+	public:
+	
+	void executeOrder();
+	private:
+};
+#endif // !DIRECTIONEVENT_H
